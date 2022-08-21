63a3b202876f5ba3881d16d1c061e10304e31721
appanand <app.anandp@gmail.com>
2022-08-21 14:48:19 +0530

    invalid data testcase of epoch date time converter

--- src/tests/EpochValidatorTest.java
+++ src/tests/EpochValidatorTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -30,10 +29,8 @@ public class EpochValidatorTest extends Testbase {
 
 	@Test
 	public void verifyEpochConversionWithValidInput() {
-		
+
 		Random r = new Random();
-		
-		
 		CloseableHttpResponse response = null;
 		System.out.println("=== start test ===");
 		System.out.println("domain - " + Testbase.domain);
@@ -42,13 +39,14 @@ public class EpochValidatorTest extends Testbase {
 		HashMap<String, String> headerMap = new HashMap<>();
 		headerMap.put("accept", "text/html,application/xhtml+xml");
 		headerMap.put("accept-encoding", "gzip, deflate, br");
-		
-		int randomEpoch = r.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);;
-		
-		String filter = "unixtimestamp="+randomEpoch;
 
-		System.out.println("=== random epoch date time - " +randomEpoch);
-		
+		int randomEpoch = r.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+		;
+
+		String filter = "unixtimestamp=" + randomEpoch;
+
+		System.out.println("=== random epoch date time - " + randomEpoch);
+
 		try {
 			response = RestClient.httpGet(domain + url, filter, headerMap);
 			actualResponseCode = response.getStatusLine().getStatusCode();
@@ -72,10 +70,50 @@ public class EpochValidatorTest extends Testbase {
 			Assert.assertTrue(true,
 					"date time format is matching to expected format yyyy-MM-dd HH:mm:ss - " + dateTime);
 
-		} catch (ParseException  e) {
+		} catch (ParseException e) {
 			Assert.assertTrue(false,
 					"date time format is not matching to expected format yyyy-MM-dd HH:mm:ss - " + dateTime);
 		}
+
+	}
+
+	@Test
+	public void verifyEpochConversionWithInvalidInput() {
+
+		Random r = new Random();
+		CloseableHttpResponse response = null;
+		System.out.println("=== start test ===");
+		System.out.println("domain - " + Testbase.domain);
+		System.out.println("uri - " + url);
+
+		HashMap<String, String> headerMap = new HashMap<>();
+		headerMap.put("accept", "text/html,application/xhtml+xml");
+		headerMap.put("accept-encoding", "gzip, deflate, br");
+
+		long randomEpoch = r.nextLong();
+		;
+
+		String filter = "unixtimestamp=" + randomEpoch;
+
+		System.out.println("=== random epoch date time - " + randomEpoch);
+
+		try {
+			response = RestClient.httpGet(domain + url, filter, headerMap);
+			actualResponseCode = response.getStatusLine().getStatusCode();
+			responseString = EntityUtils.toString(response.getEntity());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(responseString);
+		Assert.assertEquals(actualResponseCode, RESPONSE_CODE_400);
+
+		JSONObject objJson = new JSONObject(responseString);
+		String error = objJson.getString("Error");
+		System.out.println("Date time received - " + error);
+		Assert.assertEquals(error,
+				"UnixTimeService.RESTHost.fromunixtimestamp.TryCatch.Try.UnixTimeStamp: Value was either too large or too small for an Int32.");
 
 	}
 
