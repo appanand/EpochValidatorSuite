c0100164c7936364321ccbbe37f472fa48e6bbe5
appanand <app.anandp@gmail.com>
2022-08-23 21:17:29 +0530

    test cases of epoch validator

--- src/common/ExcelOperations.java
+++ src/common/ExcelOperations.java
@@ -1,11 +1,13 @@
 package common;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.HashMap;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelOperations {
@@ -15,10 +17,10 @@ public class ExcelOperations {
 
 		String url = System.getProperty("user.dir") + "//resources//testdata.xlsx";
 		try {
-			FileInputStream fis = new FileInputStream(url);
-
-			XSSFWorkbook workbook = new XSSFWorkbook(fis);
-			XSSFSheet sheet = workbook.getSheet(sheetName);
+			File excelFile = new File(url);
+			FileInputStream fis = new FileInputStream(excelFile);			
+			Workbook workbook = new XSSFWorkbook(fis);
+			Sheet sheet = workbook.getSheet(sheetName);
 			int firstRow = sheet.getFirstRowNum();
 
 			HashMap<String, Integer> testDataColumns = new HashMap<String, Integer>();
@@ -27,25 +29,25 @@ public class ExcelOperations {
 				testDataColumns.put(sheet.getRow(firstRow).getCell(i).getStringCellValue(), i);
 
 			}
-			System.out.println(testDataColumns);
+			//System.out.println(testDataColumns);
 			HashMap<String, Integer> testcases = new HashMap<String, Integer>();
 
-			System.out.println("get last row num" + sheet.getLastRowNum());
+			//System.out.println("get last row num" + sheet.getLastRowNum());
 
 			for (int i = 0; i <= sheet.getLastRowNum(); i++) {
 
 				testcases.put(sheet.getRow(i).getCell(testDataColumns.get("testcase")).getStringCellValue(), i);
 
 			}
-			System.out.println(testcases);
+			//System.out.println(testcases);
 
 			Cell dataCell = sheet.getRow(testcases.get(executedTestcase))
 					.getCell(testDataColumns.get(requiredDataColumn));
 
 			DataFormatter df = new DataFormatter();
-			
+
 			workbook.close();
-			
+
 			return df.formatCellValue(dataCell);
 
 		} catch (Exception e) {
--- src/tests/EpochValidatorTest.java
+++ src/tests/EpochValidatorTest.java
@@ -11,6 +11,8 @@ import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import common.ExcelOperations;
 import common.RestClient;
 import common.Testbase;
 
@@ -19,29 +21,27 @@ public class EpochValidatorTest extends Testbase {
 	String url = "";
 	int actualResponseCode;
 	String responseString = "";
+	String expectedMessage = "";
+	HashMap<String, String> headerMap = new HashMap<>();
 
 	@BeforeClass
 	public void setURI() {
-
 		url = prop.getProperty("uri");
-
-	}
-
-	@Test
-	public void verifyEpochConversionWithValidInput() {
-
-		Random r = new Random();
-		CloseableHttpResponse response = null;
-		System.out.println("=== start test ===");
-		System.out.println("domain - " + Testbase.domain);
-		System.out.println("uri - " + url);
-
-		HashMap<String, String> headerMap = new HashMap<>();
+		headerMap = new HashMap<>();
 		headerMap.put("accept", "text/html,application/xhtml+xml");
 		headerMap.put("accept-encoding", "gzip, deflate, br");
+	}
+
+	@Test(priority = 1)
+	public void verifyEpochConversionWithValidInput() {
+
+		String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		Random r = new Random();
+		CloseableHttpResponse response = null;
+
+		System.out.println("=== start test " + nameofCurrMethod + "===");
 
 		int randomEpoch = r.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
-		;
 
 		String filter = "unixtimestamp=" + randomEpoch;
 
@@ -77,21 +77,17 @@ public class EpochValidatorTest extends Testbase {
 
 	}
 
-	@Test
-	public void verifyEpochConversionWithInvalidInput() {
+	@Test(priority = 2)
+	public void verifyEpochConversionWithInvalidInputExceedingInt32Range() {
+
+		String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
 
 		Random r = new Random();
 		CloseableHttpResponse response = null;
-		System.out.println("=== start test ===");
-		System.out.println("domain - " + Testbase.domain);
-		System.out.println("uri - " + url);
 
-		HashMap<String, String> headerMap = new HashMap<>();
-		headerMap.put("accept", "text/html,application/xhtml+xml");
-		headerMap.put("accept-encoding", "gzip, deflate, br");
+		System.out.println("=== start test " + nameofCurrMethod + "===");
 
 		long randomEpoch = r.nextLong();
-		;
 
 		String filter = "unixtimestamp=" + randomEpoch;
 
@@ -109,11 +105,91 @@ public class EpochValidatorTest extends Testbase {
 		System.out.println(responseString);
 		Assert.assertEquals(actualResponseCode, RESPONSE_CODE_400);
 
+		expectedMessage = ExcelOperations.readDataFromExcelForTestcase("epochValidator", nameofCurrMethod,
+				"ExpectedMessage");
+
 		JSONObject objJson = new JSONObject(responseString);
 		String error = objJson.getString("Error");
-		System.out.println("Date time received - " + error);
-		Assert.assertEquals(error,
-				"UnixTimeService.RESTHost.fromunixtimestamp.TryCatch.Try.UnixTimeStamp: Value was either too large or too small for an Int32.");
+
+		Assert.assertEquals(error, expectedMessage);
+
+	}
+
+	@Test(priority = 3)
+	public void verifyEpochConversionWithInvalidInput() {
+
+		String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+
+		CloseableHttpResponse response = null;
+
+		System.out.println("=== start test " + nameofCurrMethod + "===");
+
+		String requestData = "EpochString";
+
+		String filter = "unixtimestamp=" + requestData;
+
+		try {
+			response = RestClient.httpGet(domain + url, filter, headerMap);
+			actualResponseCode = response.getStatusLine().getStatusCode();
+			responseString = EntityUtils.toString(response.getEntity());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(responseString);
+		Assert.assertEquals(actualResponseCode, RESPONSE_CODE_400);
+
+		expectedMessage = ExcelOperations.readDataFromExcelForTestcase("epochValidator", nameofCurrMethod,
+				"ExpectedMessage");
+
+		JSONObject objJson = new JSONObject(responseString);
+		String error = objJson.getString("Error");
+
+		Assert.assertEquals(error, expectedMessage);
+
+	}
+
+	@Test(priority = 4)
+	public void verifyEpochConversionWithMaxBoundaryValue() {
+
+		String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+
+		CloseableHttpResponse response = null;
+
+		System.out.println("=== start test " + nameofCurrMethod + "===");
+
+		String requestData = "" + Integer.MAX_VALUE; // to validate max possible epoch value
+
+		String filter = "unixtimestamp=" + requestData;
+
+		try {
+			response = RestClient.httpGet(domain + url, filter, headerMap);
+			actualResponseCode = response.getStatusLine().getStatusCode();
+			responseString = EntityUtils.toString(response.getEntity());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(responseString);
+		Assert.assertEquals(actualResponseCode, RESPONSE_CODE_200);
+
+		JSONObject objJson = new JSONObject(responseString);
+		String dateTime = objJson.getString("Datetime");
+		System.out.println("Date time received - " + dateTime);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+		dateFormat.setLenient(false);
+		try {
+			dateFormat.parse(dateTime.trim());
+			Assert.assertTrue(true,
+					"date time format is matching to expected format yyyy-MM-dd HH:mm:ss - " + dateTime);
+
+		} catch (ParseException e) {
+			Assert.assertTrue(false,
+					"date time format is not matching to expected format yyyy-MM-dd HH:mm:ss - " + dateTime);
+		}
 
 	}
 
